1a7b693e02d4e02dea29e851fe8b36ada559035c
Arthur Boschet <arthur.bleu25@gmail.com>
2024-06-24 13:00:36 -0400

    Updated the test.py to use the evaluation module.

--- utils/utils.py
+++ utils/utils.py
@@ -3,57 +3,73 @@ import logging
 import os
 from typing import Any, Dict
 
-import tensorflow as tf
 import torch
 
 
-def restore_checkpoint(ckpt_dir: str, state: Dict[str, Any], device: torch.device) -> Dict[str, Any]:
-  """
-  Restores a checkpoint from the given directory and returns the state dictionary.
-  
-  Parameters
-  ----------
-  ckpt_dir : str
-      The path to the checkpoint directory.
-  state : Dict[str, Any]
-      The state dictionary to restore the checkpoint to.
-  device : torch.device
-      The device to load the checkpoint on.
-  
-  Returns
-  -------
-  state : Dict[str, Any]
-      The state dictionary with the restored checkpoint.
-  """
-  if not tf.io.gfile.exists(ckpt_dir):
-    tf.io.gfile.makedirs(os.path.dirname(ckpt_dir))
-    logging.warning(f"No checkpoint found at {ckpt_dir}. "
-                    f"Returned the same state as input")
-    return state
-  else:
-    loaded_state = torch.load(ckpt_dir, map_location=device)
-    state['optimizer'].load_state_dict(loaded_state['optimizer'])
-    state['model'].load_state_dict(loaded_state['model'], strict=False)
-    state['ema'].load_state_dict(loaded_state['ema'])
-    state['step'] = loaded_state['step']
-    return state
+def restore_checkpoint(ckpt_path: str, state: Dict[str, Any], device: torch.device) -> Dict[str, Any]:
+    """
+    Restores a checkpoint from the given directory and returns the state dictionary.
+
+    Parameters
+    ----------
+    ckpt_path : str
+        The path to the checkpoint.
+    state : Dict[str, Any]
+        The state dictionary to restore the checkpoint to.
+    device : torch.device
+        The device to load the checkpoint on.
+
+    Returns
+    -------
+    state : Dict[str, Any]
+        The state dictionary with the restored checkpoint.
+    """
+    if not os.path.exists(ckpt_path):
+        os.makedirs(os.path.dirname(ckpt_path))
+        logging.warning(f"No checkpoint found at {ckpt_path}. "
+                        f"Returned the same state as input")
+        return state
+    else:
+        loaded_state = torch.load(ckpt_path, map_location=device)
+        state['optimizer'].load_state_dict(loaded_state['optimizer'])
+        state['model'].load_state_dict(loaded_state['model'], strict=False)
+        state['ema'].load_state_dict(loaded_state['ema'])
+        state['step'] = loaded_state['step']
+        return state
 
 
-def save_checkpoint(ckpt_dir: str, state: Dict[str, Any]):
-  """
-  Saves the given state dictionary to the given directory.
-  
-  Parameters
-  ----------
-  ckpt_dir : str
-      The path to the checkpoint directory.
-  state : Dict[str, Any]
-      The state dictionary to save.
-  """
-  saved_state = {
-    'optimizer': state['optimizer'].state_dict(),
-    'model': state['model'].state_dict(),
-    'ema': state['ema'].state_dict(),
-    'step': state['step']
-  }
-  torch.save(saved_state, ckpt_dir)
+def save_checkpoint(ckpt_path: str, state: Dict[str, Any]):
+    """
+    Saves the given state dictionary to the given directory.
+
+    Parameters
+    ----------
+    ckpt_path : str
+        The path to the checkpoint.
+    state : Dict[str, Any]
+        The state dictionary to save.
+    """
+    saved_state = {
+        'optimizer': state['optimizer'].state_dict(),
+        'model': state['model'].state_dict(),
+        'ema': state['ema'].state_dict(),
+        'step': state['step']
+    }
+    torch.save(saved_state, ckpt_path)
+
+
+def to_range_0_1(x: torch.Tensor) -> torch.Tensor:
+    """
+    Normalize the input tensor to the range [0, 1].
+
+    Parameters
+    ----------
+    x : torch.Tensor
+        Input tensor to be normalized.
+
+    Returns
+    -------
+    torch.Tensor
+        Normalized tensor.
+    """
+    return (x + 1.) / 2.
